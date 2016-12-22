36349a5271c1293f9e956c614d2905022b59fe98
nareshondoor <naresh.ondoor@gmail.com>
2016-12-22 18:40:09 +0530

    committing PO Status

---
+++ ondoortotal/src/main/java/com/ondoor/model/POStatus.java
@@ -0,0 +1,39 @@
+package com.ondoor.model;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class POStatus {
+
+	@Id
+	@Generated
+	private Long id;
+	
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "POStatus [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+	
+}
